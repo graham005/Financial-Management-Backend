47621da5f0070ab552ca4301b95d398cb552a70e
Enoch Graham <enochgraham003@outlook.com>
2025-04-23 13:57:24 +0300

    Data commits

---
+++ Data/ApplicationDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Financial_management_backend.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Financial_management_backend.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions options) : base(options)
+        {
+        }
+
+        public DbSet<User> Users { get; set; }
+    }
+}
