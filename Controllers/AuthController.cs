d07ca405eab2f8c0394aa5268e51e6e327df3412
Enoch Graham <enochgraham003@outlook.com>
2025-04-23 13:57:05 +0300

    Auth Controller commit

---
+++ Controllers/AuthController.cs
@@ -0,0 +1,77 @@
+﻿using Financial_management_backend.Data;
+using Financial_management_backend.Models;
+using Financial_management_backend.Services;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.IdentityModel.Tokens;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+
+namespace Financial_management_backend.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    [AllowAnonymous]
+    public class AuthController : ControllerBase
+    {
+        private readonly ApplicationDbContext _context;
+        private readonly IConfiguration _configuration;
+
+        public AuthController(ApplicationDbContext context, IConfiguration configuration)
+        {
+            _context = context;
+            _configuration = configuration;
+        }
+
+        [HttpPost("login")]
+        public IActionResult Login([FromBody] LoginRequest request)
+        {
+            var user = _context.Users.FirstOrDefault(u => u.Username == request.Username);
+
+            if (user == null || !BCrypt.Net.BCrypt.Verify(request.Password, user.Password))
+                return Unauthorized("Invalid credentials.");
+
+            var token = GenerateJwtToken(user);
+            return Ok(new { Token = token });
+        }
+
+        private string GenerateJwtToken(User user)
+        {
+            var jwtConfig = _configuration.GetSection("JwtConfig");
+            var keySection = jwtConfig["Key"];
+            var expireSection = jwtConfig["ExpireTime"];
+
+            if (string.IsNullOrEmpty(keySection))
+            {
+                throw new InvalidOperationException("JWT Key is not configured in the application settings.");
+            }
+            if (string.IsNullOrEmpty(expireSection))
+            {
+                throw new InvalidOperationException("JWT Minutes is not configured in the application settings.");
+            }
+            var key = Encoding.ASCII.GetBytes(keySection);
+
+            var claims = new List<Claim>
+            {
+                new Claim(ClaimTypes.NameIdentifier, user.Id.ToString()),
+                new Claim(ClaimTypes.Name, user.Username),
+                new Claim(ClaimTypes.Role, user.Role)
+            };
+
+            var tokenDescriptor = new SecurityTokenDescriptor
+            {
+                Subject = new ClaimsIdentity(claims),
+                Expires = DateTime.UtcNow.AddMinutes(int.Parse(expireSection)),
+                Issuer = jwtConfig["Issuer"],
+                Audience = jwtConfig["Audience"],
+                SigningCredentials = new SigningCredentials(new SymmetricSecurityKey(key), SecurityAlgorithms.HmacSha256Signature)
+            };
+
+            var tokenHandler = new JwtSecurityTokenHandler();
+            var token = tokenHandler.CreateToken(tokenDescriptor);
+            Console.WriteLine($"Token: {tokenHandler.WriteToken(token)}");
+            return tokenHandler.WriteToken(token);
+        }
+    }
+}
--- Controllers/WeatherForecastController.cs
+++ Controllers/WeatherForecastController.cs
@@ -1,9 +1,11 @@
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 
 namespace Financial_management_backend.Controllers
 {
     [ApiController]
-    [Route("[controller]")]
+    [Route("api/[controller]")]
+    [Authorize(Roles = "Admin")]
     public class WeatherForecastController : ControllerBase
     {
         private static readonly string[] Summaries = new[]
